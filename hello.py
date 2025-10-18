9824f83c7959529063a477d274fe633be526a014
dave <dolimberio.a12242640@umak.edu.ph>
2025-10-18 15:26:21 +0800

    Hello world

--- hello.py
+++ hello.py
@@ -1,12 +1,3 @@
 # hello_image.py
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-
 # Display Hello World in console
 print("Hello, World!")
-
-# Load and display the image
-img = mpimg.imread('example.jpg')
-plt.imshow(img)
-plt.axis('off')  # Hide axes
-plt.show()
